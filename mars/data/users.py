50892fd6752e11f5ca40644b972f9e8695e1822a
bezdarnij <paschabmw@mail.ru>
2026-03-18 19:08:48 +0300

    Add files via upload

---
+++ mars/data/users.py
@@ -0,0 +1,24 @@
+import datetime
+from sqlalchemy import orm
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy.orm import Session
+from . import __all_models
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+    jobs = orm.relationship("Jobs", back_populates='user')
