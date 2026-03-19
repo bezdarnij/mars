ae0cfd41ef52c1c5528ac759461edd654c7e9f6c
bezdarnij <paschabmw@mail.ru>
2026-03-19 10:55:32 +0300

    Add files via upload

--- mars/data/users.py
+++ mars/data/users.py
@@ -22,3 +22,9 @@ class User(SqlAlchemyBase):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
     jobs = orm.relationship("Jobs", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- mars/main.py
+++ mars/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,19 +10,48 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 
-def main():
-    db_session.global_init('db/mars.db')
-    db_sess = db_session.create_session()
+db_session.global_init('db/mars.db')
+db_sess = db_session.create_session()
 
-    @app.route("/")
-    def index():
-        session = db_session.create_session()
-        jobs = session.query(Jobs).all()
-        users = session.query(User).all()
-        names = {name.id: (name.surname, name.name) for name in users}
-        return render_template("index.html", jobs=jobs, names=names)
-    app.run()
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    users = session.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template("index.html", jobs=jobs, names=names)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    context = {}
+    context["title"] = "Регистрация"
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            context["form"] = form
+            context["message"] = "Такой пользователь уже есть"
+            return render_template('register.html', **context)
+        user = User(
+            email=form.login.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            speciality=form.speciality.data,
+            position=form.position.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect("/login")
+    context["form"] = form
+    return render_template('register.html', **context)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
